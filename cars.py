83f9fe11367712b5e91d297ff3b466850462177b
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2022-01-21 22:36:51 +0300

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,31 +1,36 @@
 import json
 import csv
 
-cars_list = []
 
-with open("cars.csv", 'r', encoding='utf-8') as csvfile:
-    file = csv.DictReader(csvfile, delimiter=';')
-    for line in file:
-        cars_list.append(line)
-
-cars_list.sort(key=lambda x: x['Origin'])
-
-user_input = input("Europe, US, Japan?: ")
+def file_reader():
+    interval_list = []
+    with open("cars.csv", 'r', encoding='utf-8') as file:
+        csv_file = csv.DictReader(file, delimiter=';')
+        for line in csv_file:
+            interval_list.append(line)
+            interval_list.sort(key=lambda x: x["Origin"])
+    return interval_list
 
 
 def countrySort(cars):
     return list(filter(lambda x: x['Origin'] == user_input, cars))
 
-print(countrySort(cars_list))
+
+def file_write_csv(user_input, car_list):
+    with open(f"cars_sort{user_input}.csv", 'w', encoding='utf-8') as file:
+        columns = ["Car", "MPG", "Cylinders", "Displacement", "Horsepower", "Weight", "Acceleration", "Model", "Origin"]
+        csv_file = csv.DictWriter(file, fieldnames=columns)
+        csv_file.writeheader()
+        csv_file.writerows(countrySort(car_list))
 
 
-with open("cars_sort.csv", 'w', encoding='utf-8') as csv_file:
-    columns = ["Car", "MPG", "Cylinders", "Displacement", "Horsepower", "Weight", "Acceleration", "Model", "Origin"]
-    write = csv.DictWriter(csv_file, fieldnames=columns)
-    write.writeheader()
-    write.writerows(countrySort(cars_list))
+def file_write_json(user_input, car_list):
+    with open(f"cars_sort{user_input}.json", "w", encoding='utf-8') as file:
+        for i in countrySort(car_list):
+            json_file = json.dump(i, file, indent=4)
 
-with open("cars.json", 'w', encoding='utf-8') as jsonfile:
-    for i in countrySort(cars_list):
-        json.dump(i, jsonfile, indent=4)
-        print(i)
+
+user_input = input("Europe, US, Japan?: ")
+
+file_write_csv(user_input, file_reader())
+file_write_json(user_input, file_reader())
